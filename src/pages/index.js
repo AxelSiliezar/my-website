8a50854346f5649a99a2f0b402607d9bd6424162
Axel Siliezar <amartinezsiliezar@ucmerced.edu>
2021-01-23 23:36:03 -0800

    background added and images created

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,13 +3,17 @@ import Link from "gatsby-link"
 
 const IndexPage = () => (
   <div>
-    <h1>Learn to design and code modern apps</h1>
-    <p>
-      Complete courses about the best tools and design systems. Prototype and
-      build apps with React and Swift.
-    </p>
-    <p>Now go build something great.</p>
-    <Link to="/page-2/">Watch Video</Link>
+    <div className="Hero">
+      <div className="HeroGroup">
+        <h1>Learn to design and code modern apps</h1>
+        <p>
+          Complete courses about the best tools and design systems. Prototype
+          and build apps with React and Swift.
+        </p>
+        <p>Now go build something great.</p>
+        <Link to="/page-2/">Watch Video</Link>
+      </div>
+    </div>
   </div>
 )
 
